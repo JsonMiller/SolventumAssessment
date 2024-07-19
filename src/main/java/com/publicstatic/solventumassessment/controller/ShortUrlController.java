3dc25e3bbeebe62642c500b06ba976f9f6caac18
Jason Miller <dev.jmiller@gmail.com>
2024-07-19 16:10:29 -0500

    Got the redirect working for resolving short urls.

--- src/main/java/com/publicstatic/solventumassessment/controller/ShortUrlController.java
+++ src/main/java/com/publicstatic/solventumassessment/controller/ShortUrlController.java
@@ -1,10 +1,12 @@
 package com.publicstatic.solventumassessment.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
+
 public interface ShortUrlController {
 
     /**
      * Rest endpoint that should redirect to the decoded url
      * @param encodedUrlId the id of the encoded url
      */
-    void resolveEncodedUrl(String encodedUrlId);
+    void resolveEncodedUrl(String encodedUrlId, HttpServletResponse response);
 }
--- src/main/java/com/publicstatic/solventumassessment/controller/ShortUrlControllerImpl.java
+++ src/main/java/com/publicstatic/solventumassessment/controller/ShortUrlControllerImpl.java
@@ -1,11 +1,11 @@
 package com.publicstatic.solventumassessment.controller;
 
-import com.publicstatic.solventumassessment.exceptions.InvalidUrlException;
+import com.publicstatic.solventumassessment.exceptions.InvalidIdException;
 import com.publicstatic.solventumassessment.service.EncodeService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
@@ -16,10 +16,12 @@ public class ShortUrlControllerImpl implements ShortUrlController {
 
     @Override
     @GetMapping("/{id}")
-    public void resolveEncodedUrl(@PathVariable("id") String encodedUrlId) {
+    public void resolveEncodedUrl(@PathVariable("id") String encodedUrlId, HttpServletResponse response) {
         try {
-            encodeService.decodeUrl(encodedUrlId);
-        } catch (InvalidUrlException e) {
+            String decodedUrl = encodeService.decodeId(encodedUrlId);
+            response.setHeader("Location", decodedUrl);
+            response.setStatus(302);
+        } catch (InvalidIdException e) {
             throw new RuntimeException(e);
         }
     }
